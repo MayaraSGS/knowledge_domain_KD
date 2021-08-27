83331c2c9c08d4e31134584eaebe2ee30a29bdcc
https://github.com/ <https://github.com/>
2021-08-27 07:49:01 -0300

    adição das rotas

--- frontend/kDomain/src/app/app.module.ts
+++ frontend/kDomain/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -7,6 +8,17 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { ContatoComponent } from './contato/contato.component';
+
+const appRoutes: Routes = [
+
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+
+  {path: 'menu', component: MenuComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'sobre-nos', component: SobreNosComponent},
+  {path: 'rodape', component: RodapeComponent}
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +30,8 @@ import { ContatoComponent } from './contato/contato.component';
     ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
